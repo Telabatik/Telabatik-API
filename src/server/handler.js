eac70807ac95a0b8f7c2fd01610bc664407fe52e
ferupk <feru.pratama@ui.ac.id>
2024-12-11 20:57:31 +0700

    Add most basic authentication

--- src/server/handler.js
+++ src/server/handler.js
@@ -1,7 +1,9 @@
+const { registerUser, loginUser } = require('../service/userService');
+
 async function indexHandler(request, h) {
   const response = h.response({
     status: 'success',
-    message: 'Reached Telabatik API Service',
+    message: 'Reached Telabatik API Service.',
     data: {
       accessedAt: new Date().toISOString()
     }
@@ -11,4 +13,57 @@ async function indexHandler(request, h) {
   return response;
 }
 
-module.exports = indexHandler;
+async function registerHandler(request, h) {
+  const userData = request.payload;
+  const registered = await registerUser(userData);
+
+  if (!registered) {
+    const response = h.response({
+      status: 'error',
+      message: 'User registration failed.',
+    });
+  
+    response.code(400);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'success',
+    message: 'User registered successfully.',
+    data: {
+      ...registered
+    }
+  });
+
+  response.code(201);
+  return response;
+}
+
+async function loginHandler(request, h) {
+  const userData = request.payload;
+  const loggedIn = await loginUser(userData);
+  console.log("login done");
+
+  if (!loggedIn) {
+    const response = h.response({
+      status: 'error',
+      message: 'User login failed.',
+    });
+  
+    response.code(400);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'success',
+    message: 'User logged in successfully.',
+    data: {
+      ...loggedIn
+    }
+  });
+
+  response.code(200);
+  return response;
+}
+
+module.exports = { indexHandler, registerHandler, loginHandler };
--- src/server/routes.js
+++ src/server/routes.js
@@ -1,11 +1,21 @@
-const indexHandler = require('../server/handler');
+const { indexHandler, registerHandler, loginHandler } = require('../server/handler');
  
 const routes = [
   {
     path: '/',
     method: 'GET',
     handler: indexHandler,
-  }
+  },
+  {
+    path: '/auth/register',
+    method: 'POST',
+    handler: registerHandler,
+  },
+  {
+    path: '/auth/login',
+    method: 'POST',
+    handler: loginHandler,
+  },
 ];
  
 module.exports = routes;
