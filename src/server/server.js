7667c5f4a4b76f948f8a67cf6b530988239b3684
ferupk <feru.pratama@ui.ac.id>
2024-12-12 22:24:19 +0700

    Add production deployment configurations

--- src/server/server.js
+++ src/server/server.js
@@ -8,7 +8,7 @@ const { fetchUserById } = require('../service/firestoreService');
 (async () => {
   const server = Hapi.server({
     port: 3000,
-    host: 'localhost',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
         origin: ['*']
